a0983636cf3451d54311e574142e1b5b3bd8276f
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-06-11 08:10:26 +0100

    Mon Jun 11 08:10:26 BST 2018

--- lib/solutions/sum.py
+++ lib/solutions/sum.py
@@ -1,3 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def sum(x, y):
-    raise NotImplementedError()
+    if not (0 <= x <= 100 and 0 <= y <= 100):
+        raise ValueError('Invalid arguments: {}, {}. A positive integer '
+                         'between [0, 100] is expected.'.format(x, y))
+    return x + y
+
--- test/solutions/test_sum.py
+++ test/solutions/test_sum.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 from lib.solutions.sum import sum
 
